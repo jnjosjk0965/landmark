e7ca6fb43051ddb21b4f64a53c697541be6be9c3
Jaeseok <wnwotjr8@naver.com>
2025-03-12 15:03:27 +0900

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by 주재석 on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
